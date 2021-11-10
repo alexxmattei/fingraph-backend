4f2b37730617580475732d26eefbf44170a33574
alexandru matei <alexxmattei@gmail.com>
2021-11-10 15:42:43 +0200

    added models for Nomics Coin Data

---
+++ src/main/kotlin/com/example/models/nomics/CandleData.kt
@@ -0,0 +1,14 @@
+package com.example.models.nomics
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Candle(
+    var volume: String = "",
+    var priceChange: Double = 0.0,
+    var priceChangePct: Double = 0.0,
+    var volumeChange: Double = 0.0,
+    var volumeChangePct: Double = 0.0,
+    var marketCapChange: Double = 0.0,
+    var marketCapChangePct: Double = 0.0
+)
---
+++ src/main/kotlin/com/example/models/nomics/CoinModel.kt
@@ -0,0 +1,33 @@
+package com.example.models.nomics
+
+import kotlinx.serialization.Serializable
+import java.math.BigInteger
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+@Serializable
+data class Coin(
+    var id: String = "",
+    var currency: String = "",
+    var symbol: String = "",
+    var name: String = "",
+    var logo_url: String = "",
+    var status: String = "",
+    var price: Double = 0.0,
+    var priceDate: String = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME),
+    var priceTimestamp: String = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME),
+    var circulatingSupply: String = "",
+    var maxSupply: String = "",
+    var marketCap: String = "",
+    var marketCapDominance: String = "",
+    var numExchanges: String = "",
+    var numPairs: String = "",
+    var numPairsUnmapped: String = "",
+    var firstCandle: String = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME),
+    var firstTrade: String = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME),
+    var firstOrderBook: String = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME),
+    var rank: String = "",
+    var high: Double = 0.0,
+    var highTimestamp: String = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME),
+    var oneHour: Candle = Candle()
+    )
